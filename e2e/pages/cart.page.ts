cdb278de2627e9920e44dac51def8fc191f1bfb3
Gaby Moreno <gmorenogaby@gmail.com>
2021-04-01 12:41:10 -0600

    - Add single item - Add multiples items

--- e2e/pages/cart.page.ts
+++ e2e/pages/cart.page.ts
@@ -1,8 +1,8 @@
 import { Selector } from 'testcafe';
 
 export class CartPage {
-  async getItemData() {
-    const parentSelector = `.cart_item`;
+  async getItemData(item: number) {
+    const parentSelector = `.cart_item:nth-child(${item + 2})`;
     const name = await Selector(`${parentSelector} .inventory_item_name`)
       .innerText;
     const description = await Selector(`${parentSelector} .inventory_item_desc`)
--- e2e/test/shopping-cart.spec.ts
+++ e2e/test/shopping-cart.spec.ts
@@ -1,3 +1,4 @@
+import { t } from 'testcafe';
 import { loginUser } from '../fixtures/login-user.fixture';
 import { baseurl, cartUrl } from '../fixtures/urls.fixture';
 import { validUser } from '../fixtures/user_data.fixture';
@@ -22,7 +23,7 @@ test('Navigate to the shopping cart', async t => {
   await t.expect(await getWindowLocation()).eql(cartUrl);
 });
 
-test.only('Add a single item tho the shopping cart', async t => {
+test.only('Add a single item to the shopping cart', async t => {
   const itemNumber = 1;
   const itemData = await inventoryPage.getItemData(itemNumber);
 
@@ -31,9 +32,36 @@ test.only('Add a single item tho the shopping cart', async t => {
   await inventoryPage.clickInShoppingCartLInk();
   await t.expect(await getWindowLocation()).eql(cartUrl);
 
-  const cartItemData = await cartPage.getItemData();
+  const cartItemData = await cartPage.getItemData(itemNumber);
 
   await t.expect(itemData.name).eql(cartItemData.name);
   await t.expect(itemData.description).eql(cartItemData.description);
   await t.expect(itemData.price).contains(cartItemData.price);
+});
+
+test.only('Add multiples items to the shopping cart', async t => {
+  const itemNumbers = [3, 5, 6];
+  const data = [];
+
+  for await (const itemNumber of itemNumbers) {
+    const itemData = await inventoryPage.getItemData(itemNumber);
+
+    await inventoryPage.clickAddToCart(itemNumber);
+    data.push(itemData);
+  }
+
+  await t
+    .expect(await inventoryPage.getShoppingCartBadge())
+    .eql(`${itemNumbers.length}`);
+  await inventoryPage.clickInShoppingCartLInk();
+  await t.expect(await getWindowLocation()).eql(cartUrl);
+
+  for (let index = 0; index < data.length; index++) {
+    const itemData = data[index];
+    const cartItemData = await cartPage.getItemData(index + 1);
+
+    await t.expect(itemData.name).eql(cartItemData.name);
+    await t.expect(itemData.description).eql(cartItemData.description);
+    await t.expect(itemData.price).contains(cartItemData.price);
+  }
 });
